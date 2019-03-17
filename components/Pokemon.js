a927201369fc80a95705e1737e19eca6a8d33c5b
Konstantinos Zafeiropoulos <k.a.zafeiropoulos@gmail.com>
2019-03-17 19:04:46 +0200

    Update Pokemon.js

--- components/Pokemon.js
+++ components/Pokemon.js
@@ -3,10 +3,10 @@ class Pokemon {
         this.id = data.id;
         this.name = data.name;
         this.sprite = data.sprites.front_default;
-        this.abilities = data.abilities; // More than one (arrow)
-        this.types = data.types; // More than one (arrow)
-        this.stats = data.stats; // More than one (arrow)
-        this.moves = data.moves; // More than one (arrow, many items)
+        this.abilities = data.abilities;
+        this.types = data.types; 
+        this.stats = data.stats;
+        this.moves = data.moves;
     }
 }
 
